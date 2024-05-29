28987382bd548f89e9f04cdd8829daad91619ca8
navya-krishnan <navyakrishnanplr2002@gmail.com>
2024-05-29 12:42:08 +0530

    hai

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,60 @@
+function bubbleSort(arr){
+    let swapped 
+    do{
+        swapped = false
+        for(i=0;i<arr.length;i++){
+            if(arr[i] < arr[i+1]){
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swapped = true
+            }
+        }
+    }while(swapped)
+}
+
+const arr = [45,82,19,29,59,20]
+bubbleSort(arr)
+console.log(arr);
+
+
+// function bubbleSort(a) {
+//     for (let i = 0; i < a.length; i++) {
+//         let flag = false;
+//         for (let j = 0; j < a.length - i - 1; j++) {
+//             if (a[j] > a[j + 1]) {
+//                 let temp = a[j];
+//                 a[j] = a[j + 1];
+//                 a[j + 1] = temp;
+//                 flag = true;
+//             }
+//         }
+//         if (flag === false) {
+//             break;
+//         }
+//     }
+// }
+
+// const a = [67, 29, 10, 39, 46];
+// bubbleSort(a);
+// console.log(a); 
+
+
+// function bubbleSort(arr) {
+//     let swapped
+//     do {
+//         swapped = false
+//         for (i = 0; i < arr.length; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 temp = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = temp
+//                 swapped = true
+//             }
+//         }
+//     } while (swapped)
+// }
+
+// const arr = [45, 76, 29, 30, 47, 73]
+// bubbleSort(arr)
+// console.log(arr);
---
+++ sorting/selectionSort.js
@@ -0,0 +1,15 @@
+function selectionSort(arr) {
+    for (i = 0; i < arr.length - 1; i++) {
+        for (j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
+}
+
+const arr = [34, 89, 10, -78, 0, 39]
+console.log(selectionSort(arr));
