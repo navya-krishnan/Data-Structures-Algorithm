6e90a35bc885394d9f42cafebd435acd9c229731
navya-krishnan <navyakrishnanplr2002@gmail.com>
2024-05-30 10:46:36 +0530

    bubble sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,43 +1,43 @@
-function bubbleSort(arr){
-    let swapped 
-    do{
-        swapped = false
-        for(i=0;i<arr.length;i++){
-            if(arr[i] < arr[i+1]){
-                temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
-                swapped = true
-            }
-        }
-    }while(swapped)
-}
+// function bubbleSort(arr){
+//     let swapped 
+//     do{
+//         swapped = false
+//         for(i=0;i<arr.length;i++){
+//             if(arr[i] < arr[i+1]){
+//                 temp = arr[i]
+//                 arr[i] = arr[i+1]
+//                 arr[i+1] = temp
+//                 swapped = true
+//             }
+//         }
+//     }while(swapped)
+// }
 
 const arr = [45,82,19,29,59,20]
 bubbleSort(arr)
 console.log(arr);
 
 
-// function bubbleSort(a) {
-//     for (let i = 0; i < a.length; i++) {
-//         let flag = false;
-//         for (let j = 0; j < a.length - i - 1; j++) {
-//             if (a[j] > a[j + 1]) {
-//                 let temp = a[j];
-//                 a[j] = a[j + 1];
-//                 a[j + 1] = temp;
-//                 flag = true;
-//             }
-//         }
-//         if (flag === false) {
-//             break;
-//         }
-//     }
-// }
+function bubbleSort(a) {
+    for (let i = 0; i < a.length; i++) {
+        let flag = false;
+        for (let j = 0; j < a.length - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                let temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                flag = true;
+            }
+        }
+        if (flag === false) {
+            break;
+        }
+    }
+}
 
-// const a = [67, 29, 10, 39, 46];
-// bubbleSort(a);
-// console.log(a); 
+const a = [67, 29, 10, 39, 46];
+bubbleSort(a);
+console.log(a); 
 
 
 // function bubbleSort(arr) {
