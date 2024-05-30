64b35305072716cffee154a7d0433216e2d63c56
navya-krishnan <navyakrishnanplr2002@gmail.com>
2024-05-30 12:24:09 +0530

    merge sort

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,54 +1,26 @@
-// function mergeSort(arr) {
-//     if (arr.length < 2)
-//         return arr
-
-//     const mid = Math.floor(arr.length / 2)
-//     const left = arr.slice(0, mid)
-//     const right = arr.slice(mid)
-
-//     return merge(mergeSort(left), mergeSort(right))
-// }
-
-// function merge(left, right) {
-//     sortedArray = []
-//     while (left.length && right.length) {
-//         if (left[0] <= right[0]) {
-//             sortedArray.push(left.shift())
-//         } else {
-//             sortedArray.push(right.shift())
-//         }
-//     }
-//     return [...sortedArray, ...left, ...right]
-// }
-
-
-// let arr = [8, 3, 5, 4, 7, 6, 1, 2]
-// console.log(mergeSort(arr));
-
-
-function mergeSort(arr){
-    if(arr.length < 2)
+function mergeSort(arr) {
+    if (arr.length < 2)
         return arr
 
-    const mid = Math.floor(arr.length/2)
-    const left = arr.slice(0,mid)
+    const mid = Math.floor(arr.length / 2)
+    const left = arr.slice(0, mid)
     const right = arr.slice(mid)
 
-    return merge(mergeSort(left),mergeSort(right))
+    return merge(mergeSort(left), mergeSort(right))
 }
 
-function merge(left,right){
+function merge(left, right) {
     sortedArray = []
-    while(left.length && right.length){
-        if(left[0]<=right[0]){
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
             sortedArray.push(left.shift())
-        }else{
+        } else {
             sortedArray.push(right.shift())
         }
     }
-    return [...sortedArray,...left,...right]
+    return [...sortedArray, ...left, ...right]
 }
 
 
-let arr = [67,94,2,10,25,52,10]
+let arr = [8, 3, 5, 4, 7, 6, 1, 2]
 console.log(mergeSort(arr));
